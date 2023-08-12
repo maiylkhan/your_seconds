655415eb143a69dc79e2ea2b4b6f11fe77712e1e
Maiylkhan <121551515+maiylkhan@users.noreply.github.com>
2023-08-12 19:03:31 +0400

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    char ch;
+    int n_char=0;
+    printf("Enter the text: \n");
+    while((ch=getchar())!='#'){
+        printf("  char-code : %c-%d", ch, ch);
+        n_char++;
+        if(n_char%8==0){
+            printf("\n");
+        }
+        
+    }
+    return 0;
+}
